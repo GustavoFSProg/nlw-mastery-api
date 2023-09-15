3ef549ddfc66fd569f42cf9a5ad3ae59c7ab0172
GFSprog40 <gustavo.prog40@gmail.com>
2023-09-15 12:32:01 -0300

    roteas

--- index.ts
+++ index.ts
@@ -1,10 +1,10 @@
 import { fastify } from 'fastify'
 import dotenv from 'dotenv'
 import { fastifyCors } from '@fastify/cors'
-import { getAllPromptsRoute } from './routes/get-all-prompts'
-import { uploadVideoRoute } from './routes/upload-video'
-import { createTranscriptionRoute } from './routes/create-transcription'
-import { generateAiCompletionRoute } from './routes/generate-ai-comlemtions'
+import { getAllPromptsRoute } from './src/routes/get-all-prompts'
+import { uploadVideoRoute } from './src/routes/upload-video'
+import { createTranscriptionRoute } from './src/routes/create-transcription'
+import { generateAiCompletionRoute } from './src/routes/generate-ai-comlemtions'
 
 dotenv.config()
 
